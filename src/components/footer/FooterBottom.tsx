2ab894fca5e3a2ab1ed46e6f69d7f4f65c1152e2
Shah097 <your-email@example.com>
2025-09-05 12:54:23 +0530

    update

--- src/components/footer/FooterBottom.tsx
+++ src/components/footer/FooterBottom.tsx
@@ -6,7 +6,7 @@ const FooterBottom = () => {
     <div className="spy32px flex items-center justify-center border-t border-white-3 max-md:flex-col-reverse max-md:gap-5">
       <div className="flex items-center justify-center ">
         <p className="m-text text-white-1">
-          Copyright @ {new Date().getFullYear()} <span className=" font-italic" >Interstate Removelists</span>All rights reserved
+          Copyright @ {new Date().getFullYear()} <span className=" font-italic" >Interstate Removelists.</span> All rights reserved.
         </p>
         {/* <div className="bg-black-3 max-[380px]:h-px max-[380px]:w-20 min-[380px]:h-4 min-[380px]:w-px"></div> */}
         {/* <p className="m-text">
