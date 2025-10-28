809199702066b07cf6766f7e31f7ae6523e32178
Shah097 <manveershah007@gmail.com>
2025-10-28 11:51:23 +0530

    ds

--- src/app/(inner-pages)/services/page.tsx
+++ src/app/(inner-pages)/services/page.tsx
@@ -1,11 +1,12 @@
 import Animations from "@/components/animations/Animations";
+import dynamic from "next/dynamic";
 import AboutWhyChooseUs from "@/pages/about-us/AboutWhyChooseUs";
 import HomeOneService from "@/pages/home-one/HomeOneService";
 import  InnerBanner from '@/components/ui/InnerBanner'
 import HomeOneTestimonials from "@/pages/home-one/HomeOneTestimonials";
 import { Metadata } from "next";
 import OurServices from "@/components/ui/OurService";
-import Map from "@/components/localcomponents/map"
+// import Map from "@/components/localcomponents/map"
 import OurWorks from "@/pages/home-three/OurWorks";
 import HomeTwoFaq from "@/pages/local/HomeTwoFaq";
 import ServiceDetails from "@/pages/service-details/ServiceDetailsContent"
@@ -13,6 +14,7 @@ import ServiceAbout from "@/pages/service-details/ServiceAbout"
 import LocalType from "@/components/localcomponents/LocalTypes";
 import {serviceData} from "@/data/formdata"
 import HomeOneFinancialPlanning from "@/pages/home-one/HomeOneFinancialPlanning";
+const Map = dynamic(()=>import("@/components/localcomponents/map"),{ssr:false})
 
 export const metadata: Metadata = {
   title: "Services ",
