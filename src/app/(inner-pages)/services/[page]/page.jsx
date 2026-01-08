becadbf523d81ccf16f93c38fce43b06e4784976
404manveer <manveershah007@gmail.com>
2026-01-08 16:22:48 +0530

    update

--- src/app/(inner-pages)/services/[page]/page.jsx
+++ src/app/(inner-pages)/services/[page]/page.jsx
@@ -3,7 +3,7 @@ import  InnerBanner from '@/components/ui/InnerBanner'
 import {  serviceData } from '@/data/formdata'
 import ServiceEditor from "@/pages/service/SeviceEditor"
 
-export const revalidate = 3600;
+// export const revalidate = 3600;
 export async function generateStaticParams(){
     const res = await serviceData()
     return res?.data?.map((item)=>({
@@ -17,6 +17,8 @@ export async function generateStaticParams(){
 export default async function page({params}) {
     const res = await serviceData()
     const services = res?.data?.filter(item => item.slug === params.page)[0]
+    console.log('service',services);
+    
     
     return(
         <>
--- src/app/(inner-pages)/services/page.tsx
+++ src/app/(inner-pages)/services/page.tsx
@@ -1,12 +1,12 @@
 import Animations from "@/components/animations/Animations";
 import dynamic from "next/dynamic";
-import  InnerBanner from '@/components/ui/InnerBanner'
+import InnerBanner from '@/components/ui/InnerBanner'
 import { Metadata } from "next";
 import OurWorks from "@/pages/home-three/OurWorks";
 import HomeTwoFaq from "@/pages/local/HomeTwoFaq";
-import {serviceData} from "@/data/formdata"
+import { serviceData } from "@/data/formdata"
 import HomeOneFinancialPlanning from "@/pages/home-one/HomeOneFinancialPlanning";
-const Map = dynamic(()=>import("@/components/localcomponents/map"),{ssr:false})
+const Map = dynamic(() => import("@/components/localcomponents/map"), { ssr: false })
 
 export const metadata: Metadata = {
   title: "Services ",
@@ -15,19 +15,14 @@ export const metadata: Metadata = {
 
 export default async function Services() {
   const res = await serviceData()
-  
+
   return (
     <div>
-      <InnerBanner pagename="Our Services" imgpath={'https://plus.unsplash.com/premium_photo-1661407783160-b6f950068f2a?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fG1vdmVycyUyMHNlcnZpY2VzfGVufDB8fDB8fHww'}  />
- {/* <HomeOneService /> */}
- {/* <ServiceAbout/> */}
- <HomeOneFinancialPlanning data={res?.data} className=" pt-10" show={false} />
-
-      {/* <OurServices/> */}
-    <Map/>
-    <OurWorks/>
-    <HomeTwoFaq/>
-      {/* <HomeOneTestimonials /> */}
+      <InnerBanner pagename="Our Services" imgpath={'https://plus.unsplash.com/premium_photo-1661407783160-b6f950068f2a?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fG1vdmVycyUyMHNlcnZpY2VzfGVufDB8fDB8fHww'} />
+      <HomeOneFinancialPlanning data={res?.data} className=" pt-10" show={false} />
+      <Map />
+      <OurWorks />
+      <HomeTwoFaq />
       <Animations />
     </div>
   );
